1bf7b5c4e7a3ee93d999ce349ebb4e93bcd010cb
rickyruozzi <ruozziriccardo4@gmail.com>
2024-04-20 19:08:09 +0200

    message

---
+++ start_gestionale.py
@@ -0,0 +1,12 @@
+import subprocess
+
+def switch(scelta):
+    if scelta=='1':
+        exec(open("gestionale_catalogo.py").read())
+    else:
+        print('opzione non ancora disponibile')
+
+if __name__=='__main__':
+    print('Benvenuto nel gestionale Bibliotrack, che cosa desidera fare?\n1 - Controllare o aggiornare il catalogo\n2 - controllare i prestiti attivi\n3 - gestire le tendenze\ninserire il valore corrispondente:')
+    scelta=input()
+    switch(scelta)
