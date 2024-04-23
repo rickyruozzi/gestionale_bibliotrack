893a74c575582bcbb268faa6e896e05518f936db
rickyruozzi <ruozziriccardo4@gmail.com>
2024-04-23 18:55:31 +0200

    aggiornato lo start

--- start_gestionale.py
+++ start_gestionale.py
@@ -7,7 +7,7 @@ def switch(scelta):
     if scelta=='2':
         exec(open("gestionale_prestiti.py").read())
     else:
-        print('opzione non ancora disponibile')
+        exec(open("gestionale_utenti.py").read())
 
 if __name__=='__main__':
     print('Benvenuto nel gestionale Bibliotrack, che cosa desidera fare?\n1 - Controllare o aggiornare il catalogo\n2 - controllare i prestiti attivi\n3 - gestire le tendenze\ninserire il valore corrispondente:')
